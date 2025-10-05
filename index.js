4a1b671504febd5bbcbbf43d8df923fdd2469ba4
flamewalker103-commits <ouchalesho@hotmail.com>
2025-10-05 12:42:29 -0500

    Add validation to ensure numerical input in Calculadora methods

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 class Calculadora {
   sumar(num1, num2) {
+    this.validarNumeros(num1, num2);
     return num1 + num2;
   }
 
   restar(num1, num2) {
+    this.validarNumeros(num1, num2);
     return num1 - num2;
   }
 
   multiplicar(num1, num2) {
+    this.validarNumeros(num1, num2);
     return num1 * num2;
   }
 
   dividir(num1, num2) {
+    this.validarNumeros(num1, num2);
     if (num2 === 0) throw new Error('No se puede dividir por cero');
     return num1 / num2;
+  }
+
+  validarNumeros(...numeros) {
+    numeros.forEach(num => {
+      if (typeof num !== 'number') {
+        throw new Error('Todos los argumentos deben ser números');
+      }
+    });
   }
 }
 
