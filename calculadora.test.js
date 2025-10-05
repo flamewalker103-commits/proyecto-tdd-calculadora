5d011ba4c13b5aeb78f67e1c6bbd745a4969cbf5
flamewalker103-commits <ouchalesho@hotmail.com>
2025-10-05 12:41:17 -0500

    feat: initial commit for calculadora.test.js

---
+++ calculadora.test.js
@@ -0,0 +1,29 @@
+const Calculadora = require('./index');
+
+describe('Calculadora', () => {
+  let calculadora;
+
+  beforeEach(() => {
+    calculadora = new Calculadora();
+  });
+
+  test('suma de dos números', () => {
+    expect(calculadora.sumar(1, 2)).toBe(3);
+  });
+
+  test('resta de dos números', () => {
+    expect(calculadora.restar(5, 2)).toBe(3);
+  });
+
+  test('multiplicación de dos números', () => {
+    expect(calculadora.multiplicar(3, 4)).toBe(12);
+  });
+
+  test('división de dos números', () => {
+    expect(calculadora.dividir(10, 2)).toBe(5);
+  });
+
+  test('división por cero', () => {
+    expect(() => calculadora.dividir(10, 0)).toThrow('No se puede dividir por cero');
+  });
+});
